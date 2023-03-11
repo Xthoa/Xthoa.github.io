96d75ee2927246ec90e0e4ac1a9ef6187a64fc94
Xthoa <60494693+Xthoa@users.noreply.github.com>
2023-03-11 23:18:15 +0800

    230311 modify

--- mainpage.js
+++ mainpage.js
@@ -1,12 +1,14 @@
 $(function() {
     $('#lang-sel-ch').click(function() {
         $('#lang-sel').text('中文');
+        $('title').text("徐际飞的个人主页");
         $('span.lang-ch').show();
         $('span.lang-en').hide();
     });
 
     $('#lang-sel-en').click(function() {
         $('#lang-sel').text('English');
+        $('title').text("Xthoa's homepage");
         $('span.lang-en').show();
         $('span.lang-ch').hide();
     });
